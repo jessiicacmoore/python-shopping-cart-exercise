a0af77d0ef6e1c5ae086a5f9b675c8c030eb61f2
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-13 14:36:08 -0400

    Created instance method of calculate_total_pricen for Product class

--- classes/product.py
+++ classes/product.py
@@ -1,2 +1,11 @@
 class Product:
-  
+  def __init__(self, name, base_price):
+    self.name = name
+    self.base_price = base_price
+    self.tax_rate = .13
+
+  def __str__(self):
+    return "Name: {}\nBase Price: ${:.2f}\nTax Rate: {}%".format(self.name, self.base_price, int(self.tax_rate * 100))
+
+  def calculate_total_price(self):
+    return self.base_price * (1 + self.tax_rate)
---
+++ main.py
@@ -0,0 +1,5 @@
+from classes.product import Product
+
+shampoo = Product('Shampoo', 4.00)
+print(shampoo)
+print(shampoo.calculate_total_price())
