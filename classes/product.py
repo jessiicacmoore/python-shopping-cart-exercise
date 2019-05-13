39ffca3de87af5fdb3fdfcdfcefcb2ec0c8f1593
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-13 17:28:39 -0400

    Created tax system for non_taxable, taxable, and imported

--- classes/product.py
+++ classes/product.py
@@ -1,8 +1,10 @@
+
+
 class Product:
-  def __init__(self, name, base_price):
+  def __init__(self, name, base_price, tax_type):
     self.name = name
     self.base_price = base_price
-    self.tax_rate = .13
+    self.tax_rate = tax_type
 
   def __str__(self):
     return "Name: {}\nBase Price: ${:.2f}\nTax Rate: {}%".format(self.name, self.base_price, int(self.tax_rate * 100))
--- main.py
+++ main.py
@@ -3,27 +3,24 @@ from classes.cart import ShoppingCart
 
 cart = ShoppingCart()
 
-blouse = Product('Blouse', 24.00)
-t_shirt = Product('T Shirt', 15.75)
-jeans = Product('Jeans', 51.50)
+non_taxable = 0
+taxable = 0.13
+imported = 0.20
+
+blouse = Product('Blouse', 24.00, taxable)
+t_shirt = Product('T Shirt', 15.75, non_taxable)
+jeans = Product('Jeans', 51.50, imported)
+
+
+# cart.add_to_cart(blouse)
+
+cart.add_to_cart(jeans)
+
+# cart.add_to_cart(t_shirt)
+
+# cart.remove_from_cart(jeans)
+
 
 print(cart)
-
-cart.add_to_cart(blouse)
-# print(cart)
-# print(cart.calculate_sub_total())
-# print(cart.calculate_total())
-cart.add_to_cart(jeans)
-# print(cart)
-# print(cart.calculate_sub_total())
-# print(cart.calculate_total())
-cart.add_to_cart(t_shirt)
-# print(cart)
-# print(cart.calculate_sub_total())
-# print(cart.calculate_total())
-# cart.remove_from_cart(jeans)
-# print(cart)
-# print(cart.calculate_sub_total())
-# print(cart.calculate_total())
-
+cart.print_receipt()
 print(cart.find_most_expensive())
